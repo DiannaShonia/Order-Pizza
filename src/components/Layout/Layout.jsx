62ecc86dc8ab99cc593e24d1a6a6519345c48146
Diana <Diannashonia999@gmail.com>
2021-03-13 00:36:33 +0400

    added loader

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,32 @@
 import Logo from '../Logo/Logo'
 import Header from '../Header/Header'
 import './Layout.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import DotLoader from 'react-spinners/DotLoader'
 
 const Layout = ({ children }) => {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
   return (
     <div>
       <Logo />
       <Header />
-      <div className="container">{children}</div>
+      <div className="container">
+        {loading ? (
+          <div className="loader">
+            <DotLoader color={'#000'} loading={loading} size={70} />
+          </div>
+        ) : (
+          children
+        )}
+      </div>
     </div>
   )
 }
